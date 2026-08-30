dfd68b4f8ddb2d147fc3fb96a5f0efe9122a825d
helium-ops <Mohamedmah1234@outlook.com>
2026-08-30 12:19:22 +0300

    added hover

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import ProductCards from '../components/ProductCard.jsx';
 export default function Home(){
     const products = getProducts();
     return(
-        <div className="flex flex-col justify-between items-center gap-[2vh] h-[20%] w-[100%]">
+        <div className="flex flex-col justify-between items-center gap-[2vh] h-[20%] w-[100%] ">
             <div className="flex flex-col justify-center items-center gap-[8%] pt-[3%]">
                 <h1 class="font-bold text-[2rem] mb-[2%]">Welcome to Shophub!</h1>
                 <p className="text-[#4B5563]">Discover amazing products at great prices!</p>
@@ -15,7 +15,7 @@ export default function Home(){
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 my-40 w-[85%] gap-6'>
             {products.map((product => (
-                <ProductCards product={product}/>
+                <ProductCards product={product} key={product.id}/>
             )))}
             </div>
         </div>
