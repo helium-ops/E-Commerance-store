9c4e6cd71f5f8ce29ca98b91a0e74c3d7bd1d3c1
Your Name <your.email@example.com>
2026-09-03 17:59:13 +0300

    aligned to the left

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -4,17 +4,17 @@ export default function Checkout(){
     const { getCartItemsWithProducts } = useCart();
     const cartItems = getCartItemsWithProducts();
     return(
-        <div className="checkout-page flex flex-col justify-between items-start p-4 flex-wrap gap-[2vh] h-[90%] w-[100%] h-[98vh] bg-slate-100 absolute">
-<div className="checkout-inner h-[80vh] w-auto absolute flex justify-start items-start flex-col ">
+        <div className="checkout-page flex flex-col items-start">
+            <div className="checkout-inner flex flex-col items-start">
                 <h1 className="checkout-title font-bold text-4.5">Checkout</h1>
                 <div className="checkout-summary rounded-[1vh] flex justify-start items-start">
                     <div>
                         <h2>Order summary</h2>
                         {cartItems.map((item)=>
-                        <div className="cart-item flex justify-between items-center bg-white mb-2 h-[35%] rounded-[2vh] shadow-sm w-[400%] absolute">
-                            <img src={item.product.image} alt="" className="cart-image rounded-[2vh] h-[75%] w-[35%]"/>
-                            <div className="cart-content flex justify-start items-start pb-20">
-                        <div className="cart-name-row flex justify-between items-center">
+                        <div className="cart-item">
+                            <img src={item.product.image} alt={item.product.name} className="cart-image"/>
+                            <div className="cart-content">
+                        <div className="cart-name-row">
                             <h3 className="cart-name">{item.product.name}</h3>
                             <p className="cart-price">${item.product.price}</p>
                             </div>
